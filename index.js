d0e3fb866372ea0843b625dc571cc91e669ee1f7
kadynski <73016723+kadynski@users.noreply.github.com>
2021-04-21 23:39:03 +0200

    Add files via upload
    adding initial files

---
+++ index.js
@@ -0,0 +1,9 @@
+
+for (let i =0; i<document.querySelectorAll(".drum").length; i++) {
+
+document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+
+function handleClick() {
+  alert("i got clicked!");
+}
+}
