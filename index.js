4760698ad31f7dd95f455894451f01aad07bc9c2
kadynski <73016723+kadynski@users.noreply.github.com>
2021-04-22 23:14:16 +0200

    Add files via upload
    1st update

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 
+// audio variables
+
+const tom1 = new Audio ("sounds/tom-1.mp3");
+const tom2 = new Audio ("sounds/tom-2.mp3");
+const tom3 = new Audio ("sounds/tom-3.mp3");
+const tom4 = new Audio ("sounds/tom-4.mp3");
+const tom1 = new Audio ("sounds/tom-1.mp3");
+const tom2 = new Audio ("sounds/tom-2.mp3");
+const tom2 = new Audio ("sounds/tom-2.mp3");
+
 for (let i =0; i<document.querySelectorAll(".drum").length; i++) {
 
 document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
 
-function handleClick() {
-  alert("i got clicked!");
+function handleClick(){
+
+  let = buttonInnerHTML = this.innerHTML;
+
+switch (buttonInnerHTML) {
+  case "w":
+    tom1.play ();
+    break;
+  default:
 }
 }
+ }
