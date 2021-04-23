a6323d76e83d6a55ec3e53a4404c489d776ce89d
kadynski <73016723+kadynski@users.noreply.github.com>
2021-04-23 19:53:07 +0200

    Add files via upload
    function update

--- index.js
+++ index.js
@@ -1,27 +1,65 @@
 
-// audio variables
+// audio attachment
 
 const tom1 = new Audio ("sounds/tom-1.mp3");
 const tom2 = new Audio ("sounds/tom-2.mp3");
 const tom3 = new Audio ("sounds/tom-3.mp3");
 const tom4 = new Audio ("sounds/tom-4.mp3");
-const tom1 = new Audio ("sounds/tom-1.mp3");
-const tom2 = new Audio ("sounds/tom-2.mp3");
-const tom2 = new Audio ("sounds/tom-2.mp3");
+const crash = new Audio ("sounds/crash.mp3");
+const kick = new Audio ("sounds/kick-bass.mp3");
+const snare = new Audio ("sounds/snare.mp3");
+
+
+// detecting buttons
 
 for (let i =0; i<document.querySelectorAll(".drum").length; i++) {
 
-document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
+document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
-function handleClick(){
+let = buttonInnerHTML = this.innerHTML;
+makeSound(buttonInnerHTML);
+});
 
-  let = buttonInnerHTML = this.innerHTML;
+//detecting keybord press
 
-switch (buttonInnerHTML) {
+document.addEventListener("keydown", function(event) {
+  makeSound(event.key);
+});
+
+
+//sound function
+
+function makeSound(key){
+
+
+switch (key) {
   case "w":
-    tom1.play ();
+    tom1.play();
     break;
+
+  case "a":
+      tom2.play();
+      break;
+
+  case "s":
+      tom3.play();
+      break;
+
+  case "d":
+      tom4.play();
+      break;
+
+  case "j":
+      crash.play();
+      break;
+
+  case "k":
+      kick.play();
+      break;
+
+  case "l":
+      snare.play();
+      break;
+
   default:
-}
-}
- }
+}};
